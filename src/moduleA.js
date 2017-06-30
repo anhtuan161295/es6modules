2316da780416978d9c7846c300e6bb24c174b447
QQ <anhtuan161295@gmail.com>
2017-06-30 13:03:46 +0700

    test

--- src/moduleA.js
+++ src/moduleA.js
@@ -1,7 +1,19 @@
+// Import normal
 // import {fname, lname} from './moduleB.js'
 // console.log(`${fname} ${lname}`);
 
-import {fname as f, lname as l, obj} from './moduleB.js'
-obj.name="Changed";
-console.log(obj.name);
-console.log(`${f} ${l}`);
+// Import with alias
+// import {fname as f, lname as l, obj} from './moduleB.js'
+// obj.name="Changed";
+// console.log(obj.name);
+// console.log(`${f} ${l}`);
+
+// Import default: use default as a variable
+// import {default as name} from './moduleB.js'
+// console.log(name);
+
+// Import function and class
+import {greet, Employee} from './moduleB.js'
+greet('Tedu');
+let emp = new Employee();
+emp.greeting('Welcome to Tedu');
--- src/moduleB.js
+++ src/moduleB.js
@@ -1,9 +1,28 @@
+// Export normal
 // export let fname = "Tedu";
 // export let lname = "Online Channel"
 
-let fname = "Tedu";
-let lname = "Online Channel"
-let obj ={
-    name:"123"
+// Export variable
+// let fname = "Tedu";
+// let lname = "Online Channel"
+// let obj = {
+//     name: "123"
+// }
+// export {fname,lname,obj}
+
+// Export default: export without variable name
+// let name = "Tedu";
+// export default name;
+
+// Export function and class
+export function greet(message) {
+    console.log(message);
 }
-export {fname,lname,obj}
+export class Employee {
+    constructor() {
+        console.log('Employee constructor');
+    }
+    greeting(message) {
+        console.log(message);
+    }
+}
